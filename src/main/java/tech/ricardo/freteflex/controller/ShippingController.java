1612ba375021169a8d3cb78a8f89d900befb446d
RicardoBarrote <ricardoambscfc@gmail.com>
2025-05-20 09:45:01 -0300

    Criando o controlador - ShippingController

---
+++ src/main/java/tech/ricardo/freteflex/controller/ShippingController.java
@@ -0,0 +1,25 @@
+package tech.ricardo.freteflex.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tech.ricardo.freteflex.controller.dto.ShippingResponse;
+import tech.ricardo.freteflex.service.ShippingService;
+
+@Controller
+public class ShippingController {
+
+    private final ShippingService shippingService;
+
+    public ShippingController(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
+    @GetMapping(value = "shipping/calculate")
+    public ResponseEntity<ShippingResponse> calculate(@RequestParam("type") String shippingType,
+                                                      @RequestParam("distance") Double distance,
+                                                      @RequestParam("weight") Double weight) {
+        return ResponseEntity.ok(new ShippingResponse(0.0));
+    }
+}
